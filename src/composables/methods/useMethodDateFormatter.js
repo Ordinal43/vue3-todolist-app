2fa96d6cf2568ae495bb2799285c5884c7493bd4
Bryan Ramaputra <bryan@stockbit.com>
2024-11-26 14:14:13 -0800

    improve date formatting

--- src/composables/methods/useMethodDateFormatter.js
+++ src/composables/methods/useMethodDateFormatter.js
@@ -6,10 +6,16 @@ export const useMethodDateFormatter = () => {
     return dateAdapter.format(dateValue, 'shortDate')
   }
 
-  const formatTime = (time) => {
-    // replace this with AM PM later!
-    if (time !== null) {
-      return time
+  const formatTime = (timeStr) => {
+    if (timeStr !== null) {
+      const [hour, minute] = timeStr.split(':').map(Number)
+      const period = hour >= 12 ? 'PM' : 'AM'
+      const formattedHour = hour % 12 || 12
+
+      // Include minutes only if they are not 0
+      return minute === 0
+        ? `${formattedHour} ${period}`
+        : `${formattedHour}:${minute.toString().padStart(2, '0')} ${period}`
     }
     return ''
   }
